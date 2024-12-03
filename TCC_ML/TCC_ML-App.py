d60cf02fd9e3dac731b4dcc703699d5f09a6db85
Gabriel Saraiva <gabrielsaraiva9090@gmail.com>
2024-12-02 21:55:39 -0300

    att modelo

--- TCC_ML/TCC_ML-App.py
+++ TCC_ML/TCC_ML-App.py
@@ -5,41 +5,40 @@ import io
 import base64
 from io import BytesIO
 import numpy as np
-from transformers import ViTForImageClassification, ViTImageProcessor
-from torchvision.transforms import Normalize, Resize, ToTensor, Compose
 import torch
-import boto3
-import uuid
+from torchvision.transforms import Normalize, Resize, ToTensor, Compose
 import cv2
 import requests
+import uuid
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas as rotas
 
-# Carregar o modelo salvo e o processador
-model_dir = "deepfake_vs_real_image_detection"
-model = ViTForImageClassification.from_pretrained(model_dir)
-processor = ViTImageProcessor.from_pretrained(model_dir)
+# Carregar o modelo ResNet salvo
+model_path = "best_resnet_model_complete.pth"
+model = torch.load(model_path)
+model.eval()
+
+# Configuração do dispositivo
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
 # Definir transformações
-image_mean, image_std = processor.image_mean, processor.image_std
-size = processor.size["height"]
-
-normalize = Normalize(mean=image_mean, std=image_std)
 val_transforms = Compose([
-    Resize((size, size)),
+    Resize((224, 224)),
     ToTensor(),
-    normalize
+    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# Definição do modelo
-pipe = model
+# Mapeamento de rótulos
+id2label = {0: "Real", 1: "Fake"}
+label2id = {"Real": 0, "Fake": 1}
 
+# Função para verificar se a imagem contém um rosto
 def contains_face(image):
     image_cv = np.array(image)
     image_cv = cv2.cvtColor(image_cv, cv2.COLOR_RGB2BGR)
-        
-
+    
     # Carrega o classificador Haar Cascade para detecção de rostos
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
     eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
@@ -51,14 +50,10 @@ def contains_face(image):
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     eyes = eye_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     
-    print(len(faces))
-    print(len(eyes))
-
     if len(faces) > 0 and len(eyes) >= 2:
         return True
 
     return len(eyes) > 2
-
 
 @app.route('/process_images', methods=['POST'])
 def process_image():
@@ -72,23 +67,15 @@ def process_image():
         encoded_image = base64.b64encode(image_bytes.getvalue()).decode('utf-8')
 
         # Aplicar transformações
-        image_tensor = val_transforms(image).unsqueeze(0)  # Add batch dimension
+        image_tensor = val_transforms(image).unsqueeze(0).to(device)  # Add batch dimension
         
         # Passar o tensor processado pelo modelo
         with torch.no_grad():
-
-            #image_cv = np.array(image)
-            #image_cv = cv2.cvtColor(image_cv, cv2.COLOR_RGB2BGR)
-
             if contains_face(image):
-                outputs = pipe(image_tensor)
-                logits = outputs.logits
-                predictions = torch.nn.functional.softmax(logits, dim=1)
-                confidences = predictions[0].cpu().numpy()
-                labels = model.config.id2label
-                result = {labels[i]: confidences[i] for i in range(len(labels))}
-                conf_real = result.get('Real', 0)
-                conf_fake = result.get('Fake', 0)
+                outputs = model(image_tensor)
+                probabilities = torch.nn.functional.softmax(outputs, dim=1).cpu().numpy()
+                conf_real, conf_fake = probabilities[0]
+
                 results.append({
                     'image': encoded_image,
                     'real_percentage': conf_real * 100,
@@ -96,16 +83,19 @@ def process_image():
                     'contains_face': True
                 })
 
-                name_image = f'{uuid.uuid4()}_{round(conf_real * 100)}_real_{round(conf_fake * 100)}_fake.png'
+                # Salvar a imagem no S3 com base na predição
+                name_image = f'{uuid.uuid4()}_{round(conf_real * 100)}_real_{round(conf_fake * 100)}_fake.json'
 
-
-                if conf_real>conf_fake:
-                    response = requests.put(f"https://7f3pdsi5hh.execute-api.us-east-1.amazonaws.com/dev/s3-tcc/real/{name_image}", data=encoded_image )
-                    
+                if conf_real > conf_fake:
+                    response = requests.put(
+                        f"https://7f3pdsi5hh.execute-api.us-east-1.amazonaws.com/dev/s3-tcc/real/{name_image}",
+                        data=encoded_image
+                    )
                 else:
-                    response = requests.put(f"https://7f3pdsi5hh.execute-api.us-east-1.amazonaws.com/dev/s3-tcc/fake/{name_image}", data=encoded_image )
-                
-
+                    response = requests.put(
+                        f"https://7f3pdsi5hh.execute-api.us-east-1.amazonaws.com/dev/s3-tcc/fake/{name_image}",
+                        data=encoded_image
+                    )
             else:
                 results.append({
                     'image': encoded_image,
@@ -118,4 +108,3 @@ def process_image():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
